d081ca45384d16df91f90f306f6d586a8a19d184
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-11-05 12:00:00 -0600

    TabControl

---
+++ SistemaDeReservas/MainForm.cs
@@ -0,0 +1,42 @@
+namespace SistemaDeReservas
+{
+    public partial class MainForm : Form
+    {
+        bool isMenuVisible = false;
+        FormReservar? formReservar;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AlternarVisibilidad(object sender, EventArgs e)
+        {
+            isMenuVisible = !isMenuVisible;
+            panelMenu.Visible = isMenuVisible;
+        }
+
+        private void Reservar(object sender, EventArgs e)
+        {
+            if(formReservar == null)
+            {
+                formReservar = new FormReservar();
+            }
+            MostrarFormularioEnTabPage(formReservar, tabPage1);
+        }
+
+        private void MostrarFormularioEnTabPage(Form formulario, TabPage tabPage)
+        {
+            // Configura y muestra el formulario dentro de la pestaña proporcionada
+            tabPage.Controls.Clear();
+            formulario.TopLevel = false;
+            formulario.FormBorderStyle = FormBorderStyle.None;
+            formulario.Dock = DockStyle.Fill;
+            tabPage.Controls.Add(formulario);
+            formulario.Show();
+
+            // Selecciona la pestaña actual
+            tabControlCentral.SelectedTab = tabPage;
+        }
+    }
+}
