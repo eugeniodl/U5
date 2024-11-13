eadaf84f703e0dbe95f743a7103aecf8cc07a433
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-11-13 12:04:03 -0600

    Otros Controles en Windows Forms

---
+++ ReservaHabitacionApp/ReservaHabitacion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaHabitacionApp
+{
+    public class ReservaHabitacion
+    {
+        public DateTime FechaEntrada { get; set; }
+        public int DiasEstancia { get; set; }
+        private const decimal PRECIOPORDIA = 100m;
+
+        public ReservaHabitacion(DateTime fechaEntrada,
+            int diasEstancia)
+        {
+            FechaEntrada = fechaEntrada;
+            DiasEstancia = diasEstancia;
+        }
+        public DateTime CalcularFechaSalida()
+        {
+            return FechaEntrada.AddDays(DiasEstancia);
+        }
+
+        public decimal CalcularPrecioTotal()
+        {
+            return DiasEstancia * PRECIOPORDIA;
+        }
+    }
+}
