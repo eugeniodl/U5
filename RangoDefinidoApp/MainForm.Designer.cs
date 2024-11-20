ca4d86c1ff186a48cc5d500ee703f6bb5f2e204e
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-11-19 19:40:17 -0600

    Controles de Rango Definido y Otros Controles en WinForms

---
+++ RangoDefinidoApp/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RangoDefinidoApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progressBar = new ProgressBar();
+            hScrollBar = new HScrollBar();
+            trackBar = new TrackBar();
+            lblValue = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar).BeginInit();
+            SuspendLayout();
+            // 
+            // progressBar
+            // 
+            progressBar.Dock = DockStyle.Top;
+            progressBar.Location = new Point(0, 0);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(376, 23);
+            progressBar.TabIndex = 0;
+            progressBar.Value = 50;
+            // 
+            // hScrollBar
+            // 
+            hScrollBar.Dock = DockStyle.Top;
+            hScrollBar.Location = new Point(0, 23);
+            hScrollBar.Name = "hScrollBar";
+            hScrollBar.Size = new Size(376, 17);
+            hScrollBar.TabIndex = 1;
+            hScrollBar.Value = 50;
+            hScrollBar.Scroll += hScrollBar_Scroll;
+            // 
+            // trackBar
+            // 
+            trackBar.Dock = DockStyle.Top;
+            trackBar.Location = new Point(0, 40);
+            trackBar.Maximum = 100;
+            trackBar.Name = "trackBar";
+            trackBar.Size = new Size(376, 45);
+            trackBar.TabIndex = 2;
+            trackBar.Value = 50;
+            trackBar.Scroll += trackBar_Scroll;
+            // 
+            // lblValue
+            // 
+            lblValue.AutoSize = true;
+            lblValue.Location = new Point(12, 98);
+            lblValue.Name = "lblValue";
+            lblValue.Size = new Size(86, 15);
+            lblValue.TabIndex = 3;
+            lblValue.Text = "Valor actual: 50";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(376, 138);
+            Controls.Add(lblValue);
+            Controls.Add(trackBar);
+            Controls.Add(hScrollBar);
+            Controls.Add(progressBar);
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)trackBar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar;
+        private HScrollBar hScrollBar;
+        private TrackBar trackBar;
+        private Label lblValue;
+    }
+}
---
+++ RangoDefinidoApp/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RangoDefinidoApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            UpdateValues(hScrollBar.Value);
+        }
+
+        private void UpdateValues(int value)
+        {
+            // Sincroniza los valores entre los controles
+            hScrollBar.Value = value;
+            trackBar.Value = value;
+            progressBar.Value = value;
+
+            // Actualiza el Label
+            lblValue.Text = $"Valor actual: {value}";
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            UpdateValues(trackBar.Value);
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+            UpdateValues(progressBar.Value);
+        }
+    }
+}
